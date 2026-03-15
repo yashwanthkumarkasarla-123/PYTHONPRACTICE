cad9c5a5f30b22b9fc1a9498d7494d7763fc5eef
yashwanth kumar <yashwanth.kasarla@pearson.com>
2026-03-15 09:18:08 +0530

    added day1 python file

---
+++ day1.py
@@ -0,0 +1,21 @@
+str1 = "Hello"
+Str2 = "This is my first python file"
+result= str1 +" "+Str2
+
+print(result)
+
+
+
+str='5'
+str2='10'
+myage = str+str2
+print(myage)
+#if number is quoted then it stores as string 
+# a number should be represented wihtout quotes so it takes as number
+
+
+
+str=5
+str21=10
+myage=str+str21
+print(myage)
